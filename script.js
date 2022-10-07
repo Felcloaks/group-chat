283aeb5bdb4e44b9e9b82111d76e847db9b34183
HarJa <85579713+harjakrepp@users.noreply.github.com>
2022-10-08 02:04:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function groupParticipants(){
+	var owner = ["Owner"];
+	var ownername = ["Sandira"];
+	var admin = ["Admin"];
+	var adminlist = ["Sandira","Harjaa","Erland","Hadyan"];
+	var member = ["Member"];
+	var membercount = ["54"];
+	var bot = ["Bot"];
+	var botlist = ["Zeta BOT","Hina BOT","Aizen BOT"];
+	
+	document.getElementById('owner').innerHTML = owner[0];
+	document.getElementById('admin').innerHTML = admin[0];
+	document.getElementById('member').innerHTML = member[0];
+	document.getElementById('bot').innerHTML = bot[0];
+	document.getElementById('ownerName').innerHTML = ownername[0];
+	document.getElementById('admin1').innerHTML = adminlist[0];
+	document.getElementById('memberCount').innerHTML = membercount[0];
+	document.getElementById('bot1').innerHTML = botlist[0];
+	document.getElementById('admin2').innerHTML = adminlist[1];
+	document.getElementById('bot2').innerHTML = botlist[1];
+	document.getElementById('admin3').innerHTML = adminlist[2];
+	document.getElementById('bot3').innerHTML = botlist[2];
+	document.getElementById('admin4').innerHTML = adminlist[3];
+}
